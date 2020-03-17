71c23cd340502aac6cc6ce2652258d4c17835b81
Kike Sanchez <kikin1601@hotmail.com>
2020-03-17 11:37:43 -0500

    actualizar imagen de usuario

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -67,11 +67,44 @@ app.put('/upload/:tipo/:id', function(req, res) {
         err
       });
 
-    res.json({
+    imagenUsuario(id, res, nombreArchivo);
+    /* res.json({
       ok: true,
       message: 'Imagen subida correctamente'
-    });
+    }); */
   });
 });
+
+function imagenUsuario(id, res, nombreArchivo) {
+  Usuario.findById(id, (err, usuarioDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err
+      });
+    }
+
+    if (!usuarioDB) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: 'Usuario no existe'
+        }
+      });
+    }
+
+    usuarioDB.img = nombreArchivo;
+
+    usuarioDB.save((err, usuarioGuardado) => {
+      res.json({
+        ok: true,
+        usuario: usuarioGuardado,
+        img: nombreArchivo
+      });
+    });
+  });
+}
+
+function imagenProducto() {}
 
 module.exports = app;
