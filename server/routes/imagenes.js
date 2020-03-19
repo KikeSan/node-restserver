56d35f459c1dc7265ab9452d43e941410a4e9efa
Kike Sanchez <kikin1601@hotmail.com>
2020-03-19 18:20:47 -0500

    Subida de archivos

--- server/routes/imagenes.js
+++ server/routes/imagenes.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { verificaTokenImg } = require('../middlewares/autenticacion');
 
 let app = express();
 
-app.get('/imagen/:tipo/:img', (req, res) => {
+app.get('/imagen/:tipo/:img', verificaTokenImg, (req, res) => {
   let tipo = req.params.tipo;
   let img = req.params.img;
 
-  let pathImg = `./uploads/${tipo}/${img}`;
+  //let pathImg = `./uploads/${tipo}/${img}`;
 
-  //path.resolve recibe la ruta, __dirname indica la raiz del app y seguido de coma la ruta
-  let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+  let pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${img}`);
 
-  res.sendFile(noImagePath);
+  if (fs.existsSync(pathImagen)) {
+    res.sendFile(pathImagen);
+  } else {
+    //path.resolve recibe la ruta, __dirname indica la raiz del app y seguido de coma la ruta
+    let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+    res.sendFile(noImagePath);
+  }
 });
 
 module.exports = app;
